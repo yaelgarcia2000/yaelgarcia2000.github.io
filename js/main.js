07fdbffca232a4921e7712b79ee3c92221b74e79
yaelgarcia2000 <yaelgc2000@gmail.com>
2022-10-27 06:32:15 -0500

    reproduccion de video

--- js/main.js
+++ js/main.js
@@ -1,3 +1,75 @@
+function haltEvent(event) {
+  event.preventDefault && event.preventDefault();
+  event.stopPropagation && event.stopPropagation();
+  event.cancelBubble = true;
+  event.returnValue = false;
+  return false;
+}
+// prevent right-click menus from appearing
+document.addEventListener("contextmenu", haltEvent);
+// more attempts to prevent right-click menus from appearing -- avoid; they block touchscreen/iPad controls
+// document.addEventListener("touchmove", haltEvent );
+// document.addEventListener("touchcancel", haltEvent );
+
+// add events for both touch and mouse controls
+function addButtonEventListeners(
+  buttonElementID,
+  startFunction,
+  endFunction
+) {
+  let element = document.getElementById(buttonElementID);
+  element.addEventListener("touchstart", startFunction);
+  element.addEventListener("mousedown", startFunction);
+  element.addEventListener("touchend", endFunction);
+  element.addEventListener("mouseup", endFunction);
+}
+function associateMovementControls(buttonElementID, component, keyName) {
+  addButtonEventListeners(
+    buttonElementID,
+    function (event) {
+      component.registerKeyDown(keyName);
+      return haltEvent(event);
+    },
+    function (event) {
+      component.registerKeyUp(keyName);
+      return haltEvent(event);
+    }
+  );
+}
+// need to run javascript code after a-scene entities and components are loaded
+AFRAME.registerComponent("screen-controls", {
+  init: function () {
+    let component =
+      document.getElementById("camera").components[
+        "extended-wasd-controls"
+      ];
+
+    associateMovementControls(
+      "buttonMoveForward",
+      component,
+      component.data.moveForwardKey
+    );
+    associateMovementControls(
+      "buttonMoveBackward",
+      component,
+      component.data.moveBackwardKey
+    );
+    associateMovementControls(
+      "buttonMoveLeft",
+      component,
+      component.data.moveLeftKey
+    );
+    associateMovementControls(
+      "buttonMoveRight",
+      component,
+      component.data.moveRightKey
+    );
+  },
+  tick: function (time, deltaTime) {},
+});
+// disable press "F" to enter fullscreen mode
+//disable WASD controls attached to default camera -->
+
 AFRAME.registerComponent("a1-1a-d1-v1-f-pp", {
   init: function () {
     var Video = document.querySelector("#a1-1a-d1-v1");
