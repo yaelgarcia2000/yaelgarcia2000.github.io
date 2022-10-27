21704e8abd461ece64e910fdc4117d847197e256
yaelgarcia2000 <yaelgc2000@gmail.com>
2022-10-27 06:24:41 -0500

    reproduccion de video

--- js/main.js
+++ js/main.js
@@ -1,91 +1,295 @@
-
-
-
-
 AFRAME.registerComponent("a1-1a-d1-v1-f-pp", {
   init: function () {
-    var Video1 = document.querySelector("#a1-1a-d1-v1");
-    var Controls1 = document.querySelector("#a1-1a-d1-v1-controls");
+    var Video = document.querySelector("#a1-1a-d1-v1");
+    var Controls = document.querySelector("#a1-1a-d1-v1-controls");
     this.el.addEventListener("click", function () {
-      if (Video1.paused) {
-        Video1.play();
-        Controls1.setAttribute("src", "#a1-1a-d1-v1-i-play");
+      if (Video.paused) {
+        Video.play();
+        Controls.setAttribute("src", "#play");
       } else {
-        Video1.pause();
-        Controls1.setAttribute("src", "#a1-1a-d1-v1-i-pause");
+        Video.pause();
+        Controls.setAttribute("src", "#pause");
       }
     });
   },
 });
-AFRAME.registerComponent("play-pause2", {
+AFRAME.registerComponent("a1-1a-d1-v2-f-pp", {
   init: function () {
-    var myVideo2 = document.querySelector("#video2");
-    var videoControls2 = document.querySelector("#videoControls2");
+    var Video = document.querySelector("#a1-1a-d1-v2");
+    var Controls = document.querySelector("#a1-1a-d1-v2-controls");
     this.el.addEventListener("click", function () {
-      if (myVideo2.paused) {
-        myVideo2.play();
-        videoControls2.setAttribute("src", "#pause2");
+      if (Video.paused) {
+        Video.play();
+        Controls.setAttribute("src", "#play");
       } else {
-        myVideo2.pause();
-        videoControls2.setAttribute("src", "#play2");
+        Video.pause();
+        Controls.setAttribute("src", "#pause");
       }
     });
   },
 });
-AFRAME.registerComponent("play-pause3", {
+AFRAME.registerComponent("a1-1a-d1-v3-f-pp", {
   init: function () {
-    var myVideo3 = document.querySelector("#video3");
-    var videoControls3 = document.querySelector("#videoControls3");
+    var Video = document.querySelector("#a1-1a-d1-v3");
+    var Controls = document.querySelector("#a1-1a-d1-v3-controls");
     this.el.addEventListener("click", function () {
-      if (myVideo3.paused) {
-        myVideo3.play();
-        videoControls3.setAttribute("src", "#pause3");
+      if (Video.paused) {
+        Video.play();
+        Controls.setAttribute("src", "#play");
       } else {
-        myVideo3.pause();
-        videoControls3.setAttribute("src", "#play3");
+        Video.pause();
+        Controls.setAttribute("src", "#pause");
       }
     });
   },
 });
-AFRAME.registerComponent("play-pause4", {
+AFRAME.registerComponent("a1-1a-d1-v4-f-pp", {
   init: function () {
-    var myVideo4 = document.querySelector("#video4");
-    var videoControls4 = document.querySelector("#videoControls4");
+    var Video = document.querySelector("#a1-1a-d1-v4");
+    var Controls = document.querySelector("#a1-1a-d1-v4-controls");
     this.el.addEventListener("click", function () {
-      if (myVideo4.paused) {
-        myVideo4.play();
-        videoControls4.setAttribute("src", "#pause4");
+      if (Video.paused) {
+        Video.play();
+        Controls.setAttribute("src", "#play");
       } else {
-        myVideo4.pause();
-        videoControls4.setAttribute("src", "#play4");
+        Video.pause();
+        Controls.setAttribute("src", "#pause");
       }
     });
   },
 });
-AFRAME.registerComponent("play-pause5", {
+AFRAME.registerComponent("a1-1a-d1-v5-f-pp", {
   init: function () {
-    var myVideo5 = document.querySelector("#video5");
-    var videoControls5 = document.querySelector("#videoControls5");
+    var Video = document.querySelector("#a1-1a-d1-v5");
+    var Controls = document.querySelector("#a1-1a-d1-v5-controls");
     this.el.addEventListener("click", function () {
-      if (myVideo5.paused) {
-        myVideo5.play();
-        videoControls5.setAttribute("src", "#pause5");
+      if (Video.paused) {
+        Video.play();
+        Controls.setAttribute("src", "#play");
       } else {
-        myVideo5.pause();
-        videoControls5.setAttribute("src", "#play5");
+        Video.pause();
+        Controls.setAttribute("src", "#pause");
       }
     });
   },
 });
 
-AFRAME.registerComponent("play-curve", {
-  init: function () {
-    var pantallacurva1 = document.querySelector("#pantallacurva1");
-    if (pantallacurva1.paused) {
-      pantallacurva1.play();
-    }
-  },
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 document.querySelector("a-scene").object3D;
 
