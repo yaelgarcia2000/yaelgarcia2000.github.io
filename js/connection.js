a203225ebc8a02f2c172c97c5032f2f399a4541f
yaelgarcia2000 <yaelgc2000@gmail.com>
2022-12-22 05:02:11 -0600

    conection

--- js/connection.js
+++ js/connection.js
@@ -9,7 +9,14 @@
             //console.log("éxito");
             let data = JSON.parse(xhr.responseText);
 
-            document.getElementById("p-e-a1-i").setAttribute("src", data[0].id_pub_sis);
+            for(i=0;i<=48;i++){
+
+                document.getElementById(`${data[i].id_obj_entorno}`).setAttribute("src", data[i].id_pub_sis);
+
+            }
+          //
+
+        /*  document.getElementById("p-e-a1-i").setAttribute("src", data[0].id_pub_sis);
             document.getElementById("p-e-b1-v").setAttribute("src", data[1].id_pub_sis);
             document.getElementById("p-e-c1-i").setAttribute("src", data[2].id_pub_sis);
             document.getElementById("p-o-a1-v").setAttribute("src", data[3].id_pub_sis);
@@ -43,22 +50,22 @@
             document.getElementById("p9-cb-e-c2-i").setAttribute("src", data[29].id_pub_sis);
             document.getElementById("p10-cb-e-d1-v").setAttribute("src", data[30].id_pub_sis);
             document.getElementById("p11-cb-e-d2-i").setAttribute("src", data[31].id_pub_sis);
-            document.getElementById("p12-cb-o-a0-v").setAttribute("src", data[32].id_pub_sis);
-            document.getElementById("p13-cb-o-a1-i").setAttribute("src", data[33].id_pub_sis);
-            document.getElementById("p14-cb-o-a2-v").setAttribute("src", data[34].id_pub_sis);
-            document.getElementById("p15-cb-o-a3-i").setAttribute("src", data[35].id_pub_sis);
-            document.getElementById("p16-cb-o-a4-v").setAttribute("src", data[36].id_pub_sis);
-            document.getElementById("p17-cb-o-a5-i").setAttribute("src", data[37].id_pub_sis);
-            document.getElementById("p18-cb-o-b1-v").setAttribute("src", data[38].id_pub_sis);
-            document.getElementById("p19-cb-o-b2-i").setAttribute("src", data[39].id_pub_sis);
-            document.getElementById("p20-cb-o-b3-v").setAttribute("src", data[40].id_pub_sis);
-            document.getElementById("p21-cb-o-b4-i").setAttribute("src", data[41].id_pub_sis);
-            document.getElementById("p22-cb-o-c1-v").setAttribute("src", data[42].id_pub_sis);
-            document.getElementById("p23-cb-o-c2-i").setAttribute("src", data[43].id_pub_sis);
-            document.getElementById("p24-cb-o-d1-v").setAttribute("src", data[44].id_pub_sis);
-            document.getElementById("p25-cb-o-e1-i").setAttribute("src", data[45].id_pub_sis);
-            document.getElementById("p26-cb-o-e2-v").setAttribute("src", data[46].id_pub_sis);
-            document.getElementById("p27-cb-o-e2-i").setAttribute("src", data[47].id_pub_sis);
+            document.getElementById("p12-cb-o-a1-v").setAttribute("src", data[32].id_pub_sis);
+            document.getElementById("p13-cb-o-a2-i").setAttribute("src", data[33].id_pub_sis);
+            document.getElementById("p14-cb-o-a3-v").setAttribute("src", data[34].id_pub_sis);
+            document.getElementById("p15-cb-o-a4-i").setAttribute("src", data[35].id_pub_sis);
+            document.getElementById("p16-cb-o-a5-v").setAttribute("src", data[36].id_pub_sis);
+            document.getElementById("p17-cb-o-a6-i").setAttribute("src", data[37].id_pub_sis);
+            document.getElementById("p18-cb-o-a7-v").setAttribute("src", data[38].id_pub_sis);
+            document.getElementById("p19-cb-o-b1-i").setAttribute("src", data[39].id_pub_sis);
+            document.getElementById("p20-cb-o-b2-v").setAttribute("src", data[40].id_pub_sis);
+            document.getElementById("p21-cb-o-b3-i").setAttribute("src", data[41].id_pub_sis);
+            document.getElementById("p22-cb-o-b4-v").setAttribute("src", data[42].id_pub_sis);
+            document.getElementById("p23-cb-o-c1-i").setAttribute("src", data[43].id_pub_sis);
+            document.getElementById("p24-cb-o-c2-v").setAttribute("src", data[44].id_pub_sis);
+            document.getElementById("p25-cb-o-d1-i").setAttribute("src", data[45].id_pub_sis);
+            document.getElementById("p26-cb-o-e1-v").setAttribute("src", data[46].id_pub_sis);
+            document.getElementById("p27-cb-o-e2-i").setAttribute("src", data[47].id_pub_sis); */
 
         } else {
             //console.log("Error");
